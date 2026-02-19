ff8fa8ce25dc04dace0a14e1aaba6ad76cc53b8e
Bhanu Prakash <bhanubalineni06@gmail.com>
2026-02-19 09:07:45 +0530

    Added usecasename

---
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+public class PalindromeCheckerApp {
+
+    /*
+    UC1
+    @author Developer
+    @version 1.0
+    */
+
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System intitialized successfully");
+    }
+}
+
