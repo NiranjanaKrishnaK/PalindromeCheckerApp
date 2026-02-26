c58173611ad8671c92655d7774bd0740300a4da0
NiranjanaKrishnaK <niranjanakumar2510@gmail.com>
2026-02-26 09:07:02 +0530

    Niranjana added UC8: Linked List Based Palindrome Checker

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,14 +1,24 @@
 public class PalindromeCheckerApp {
 
     /*
-    UC7: Deque-Based Optimized Palindrome Checker
+    UC8: Linked List Based Palindrome Checker
     @author Developer
-    @version 7.0
+    @version 8.0
     */
+
+    // Node class for singly linked list
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version : 7.0");
+        System.out.println("Version : 8.0");
         System.out.println("System initialized successfully");
 
         java.util.Scanner scanner = new java.util.Scanner(System.in);
@@ -25,18 +35,42 @@ public class PalindromeCheckerApp {
     }
     public static boolean isPalindrome(String input) {
         String normalized = input.replaceAll("\\s+", "").toLowerCase();
-        java.util.Deque<Character> deque = new java.util.LinkedList<>();
+        Node head = null, tail = null;
         for (char ch : normalized.toCharArray()) {
-            deque.addLast(ch);
-        }
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
-
-            if (front != rear) {
-                return false;
+            Node newNode = new Node(ch);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
             }
         }
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
         return true;
+    }
+    private static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
     }
 }
